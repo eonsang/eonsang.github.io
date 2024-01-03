023105a2b213923f98962a755a8184fb65656a34
eonsang <2eonhworking@gmail.com>
2024-01-03 13:48:34 +0900

    book:object 작성중

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  // { href: '/books', title: 'Books' },
+  { href: '/books', title: 'Books' },
   // { href: '/resume', title: 'Resume' },
 ]
 
